8c6af0877d83306bde670b25e1a6168a57e2dd44
gmatasruiz <guillermo.matas27>
2024-05-05 20:09:21 +0200

    refactor(autodetect llm library): Auto-select MLX or Transformers functions depending on torch.backends device

--- prompt_creation/classes/BaseLLMInstance.py
+++ prompt_creation/classes/BaseLLMInstance.py
@@ -5,24 +5,13 @@ import os
 from abc import ABC, abstractmethod
 import transformers
 import mlx_lm
+import torch
 
 from dotenv import load_dotenv
 
 
 # --- Classes ---
 class BaseLLMInstance(ABC):
-    """
-    @Citing MLX
-    The MLX software suite was initially developed with equal contribution by Awni Hannun, Jagrit Digani, Angelos Katharopoulos, and Ronan Collobert. If you find MLX useful in your research and wish to cite it, please use the following BibTex entry:
-
-    @software{mlx2023,
-      author = {Awni Hannun and Jagrit Digani and Angelos Katharopoulos and Ronan Collobert},
-      title = {{MLX}: Efficient and flexible machine learning on Apple silicon},
-      url = {https://github.com/ml-explore},
-      version = {0.0},
-      year = {2023},
-    }
-    """
 
     def __init__(self, model_name: str, model_id: str):
 
@@ -31,73 +20,59 @@ class BaseLLMInstance(ABC):
 
         self.model_name = model_name
         self.model_id = model_id
-        self.pipeline = None
+        self.device = self.get_model_device()
         self.hf_token = os.getenv("HF_TOKEN")
         self.init_model()
 
+    def get_model_device(self, device_map: str = "auto"):
+        if torch.backends.mps.is_available():
+            return torch.device("mps")
+        else:
+            # If device_map is 'auto', let's decide the device based on whether CUDA is available
+            return torch.device(
+                "cuda"
+                if torch.cuda.is_available() and device_map == "auto"
+                else "cpu"
+            )
+
     def init_model(
         self,
-        device_map: str = "auto",
         model_kwargs: dict = {},
     ):
-
         logging.basicConfig(level=logging.INFO)
-        # Using a pipeline for text generation with the model
+        if self.device == torch.device("mps"):
+            """
+            @Citing MLX
+            The MLX software suite was initially developed with equal contribution by Awni Hannun, Jagrit Digani, Angelos Katharopoulos, and Ronan Collobert. If you find MLX useful in your research and wish to cite it, please use the following BibTex entry:
 
-        self.pipeline = transformers.pipeline(
-            task="text-generation",
-            model=self.model_id,
-            device_map=device_map,
-            model_kwargs=model_kwargs,
-            token=self.hf_token,
-        )
+            @software{mlx2023,
+            author = {Awni Hannun and Jagrit Digani and Angelos Katharopoulos and Ronan Collobert},
+            title = {{MLX}: Efficient and flexible machine learning on Apple silicon},
+            url = {https://github.com/ml-explore},
+            version = {0.0},
+            year = {2023},
+            }
+            """
 
-    def read_prompt(self, source_file_path: str, template_file_path: str):
-        """
-        Reads and merges source and template JSON data.
+            self.model, self.tokenizer = mlx_lm.load(
+                self.model_id, tokenizer_config=model_kwargs
+            )
 
-        Args:
-            source_file_path (str): The path to the source JSON file.
-            template_file_path (str): The path to the template JSON file.
+        else:
+            # Load the tokenizer and model
+            self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+                self.model_id,
+                token=self.hf_token,
+                ignore_mismatched_sizes=True,
+            )
+            self.model = transformers.AutoModelForCausalLM.from_pretrained(
+                self.model_id,
+                token=self.hf_token,
+                **model_kwargs,
+                ignore_mismatched_sizes=True
+            )
 
-        Returns:
-            str: The merged prompt text.
-        """
-        # Load the source JSON content
-        with open(source_file_path, "r") as file:
-            source_json = json.load(file)
-        source_data = source_json["data"]
-
-        # Load the template JSON content
-        with open(template_file_path, "r") as file:
-            template_json = json.load(file)
-        template_data = template_json["data"]
-
-        # Replace the placeholder with the actual source data
-        merged_prompt = template_data.replace(r"%%source", source_data)
-
-        return merged_prompt
-
-    @abstractmethod
-    def process_prompt(
-        self, source_file_path: str, template_file_path: str, **llm_kwargs
-    ):
-        pass
-
-    def write_response(self, filepath: str, response: str):
-        with open(filepath, "w") as file:
-            json.dump({"response": response}, file, indent=4)
-
-
-class AppleSiliconBaseLLMInstance(BaseLLMInstance):
-    def __init__(self, model_name: str, model_id: str):
-        super().__init__(model_name=model_name, model_id=model_id)
-
-    def init_model(self):
-
-        logging.basicConfig(level=logging.INFO)
-        # Using a pipeline for text generation with the model
-        self.model, self.tokenizer = mlx_lm.load(self.model_id)
+            self.model.to(self.device)
 
     def read_prompt(self, source_file_path: str, template_file_path: str):
         """
--- prompt_creation/classes/SpecificLLMInstance.py
+++ prompt_creation/classes/SpecificLLMInstance.py
@@ -1,31 +1,25 @@
 # --- Imports ---
 import torch
+import transformers
 import mlx_lm
 
 
-from BaseLLMInstance import BaseLLMInstance, AppleSiliconBaseLLMInstance
+from BaseLLMInstance import BaseLLMInstance
+
 
 # --- Constants ---
 MODEL_DEF = {
     "mixtral-8x7b": {
         "display_name": "Mixtral 8x7B",
         "name": "mixtral-8x-7b",
-        "hf_id": "mistralai/Mixtral-8x7B-v0.1",
-    },
-    "applesilicon_mixtral-8x7b": {
-        "display_name": "(Apple Silicon) Mixtral 8x7B",
-        "name": "AppleS_mixtral-8x-7b",
-        "hf_id": "mlx-community/Mixtral-8x7B-v0.1-hf-4bit-mlx",
+        "basemodel_hf_id": "mistralai/Mixtral-8x7B-v0.1",
+        "mlx_hf_id": "mlx-community/Mixtral-8x7B-Instruct-v0.1-4bit",
     },
     "meta-llama-3-8b": {
         "display_name": "Meta Llama3 8B",
         "name": "Meta-Llama-3-8B",
-        "hf_id": "meta-llama/Meta-Llama-3-8B",
-    },
-    "applesilicon_meta-llama-3-8b": {
-        "display_name": "(Apple Silicon) Meta Llama3 8B",
-        "name": "AppleS_Meta-Llama-3-8B",
-        "hf_id": "mlx-community/Meta-Llama-3-8B-Instruct-4bit",
+        "basemodel_hf_id": "meta-llama/Meta-Llama-3-8B",
+        "mlx_hf_id": "mlx-community/Meta-Llama-3-8B-Instruct-4bit",
     },
 }
 
@@ -53,11 +47,21 @@ class ChatGPTInstance(BaseLLMInstance):
 
 
 class Mixtral8x7BInstance(BaseLLMInstance):
-    def __init__(self):
+    def __init__(self, use_mlx_model=True):
+        # Set model data
         self.model_info = MODEL_DEF["mixtral-8x7b"]
-        super().__init__(
-            model_name=self.model_info["name"], model_id=self.model_info["hf_id"]
+
+        # Use appropriate model from Hugging Face -> Quantized has lower precision but better performance
+        model_id = (
+            self.model_info["mlx_hf_id"]
+            if use_mlx_model
+            else self.model_info["basemodel_hf_id"]
         )
+
+        super().__init__(model_name=self.model_info["name"], model_id=model_id)
+
+    def get_model_device(self, device_map: str = "auto"):
+        return super().get_model_device(device_map)
 
     def init_model(
         self,
@@ -67,13 +71,28 @@ class Mixtral8x7BInstance(BaseLLMInstance):
     ):
 
         # 4-bit quantization configuration -- Not available on devices with no CUDA support -
-        # nf4_config = transformers.BitsAndBytesConfig(
-        #     load_in_4bit=True,
-        #     bnb_4bit_quant_type="nf4",
-        #     bnb_4bit_use_double_quant=True,
-        #     bnb_4bit_compute_dtype=torch.bfloat16,
-        # )
-        # model_kwargs = model_kwargs | {"quantization_config": nf4_config}
+        if torch.cuda.is_available() and self.device == torch.device("cuda"):
+            nf4_config = transformers.BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_compute_dtype=torch.bfloat16,
+            )
+            model_kwargs = model_kwargs | {"quantization_config": nf4_config}
+
+        else:
+            # MLX config
+            model_kwargs = model_kwargs | {
+                "dim": 4096,
+                "n_layers": 32,
+                "head_dim": 128,
+                "hidden_dim": 14336,
+                "n_heads": 32,
+                "n_kv_heads": 8,
+                "norm_eps": 1e-05,
+                "vocab_size": 32000,
+                "moe": {"num_experts_per_tok": 2, "num_experts": 8},
+            }
 
         super().init_model(model_kwargs=model_kwargs)
 
@@ -81,7 +100,25 @@ class Mixtral8x7BInstance(BaseLLMInstance):
         return super().read_prompt(source_file_path, template_file_path)
 
     def process_prompt(self, prompt: str, **llm_kwargs):
-        response = self.pipeline(prompt, **llm_kwargs)[0]["generated_text"]
+
+        messages = [{"role": "user", "content": prompt}]
+        input_ids = self.tokenizer.apply_chat_template(messages)
+
+        if self.device == torch.device("mps"):
+            # Using MLX
+            prompt = self.tokenizer.decode(input_ids)
+            response = mlx_lm.generate(
+                self.model, self.tokenizer, prompt=prompt, **llm_kwargs
+            )
+        else:
+            # Using transformers
+
+            # Generate response using the model
+            outputs = self.model.generate(input_ids, **llm_kwargs)
+
+            # Decode and return the generated text
+            response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
         return response
 
     def write_response(self, filepath: str, response: str):
@@ -89,15 +126,27 @@ class Mixtral8x7BInstance(BaseLLMInstance):
 
 
 class LlamaV38BInstance(BaseLLMInstance):
-    def __init__(self):
+    def __init__(self, use_mlx_model=True):
+        # Set model data
         self.model_info = MODEL_DEF["meta-llama-3-8b"]
-        super().__init__(
-            model_name=self.model_info["name"], model_id=self.model_info["hf_id"]
+
+        # Use appropriate model from Hugging Face -> Quantized has lower precision but better performance
+        model_id = (
+            self.model_info["mlx_hf_id"]
+            if use_mlx_model
+            else self.model_info["basemodel_hf_id"]
         )
+
+        super().__init__(model_name=self.model_info["name"], model_id=model_id)
+
+    def get_model_device(self, device_map: str = "auto"):
+        return super().get_model_device(device_map)
 
     def init_model(
         self,
-        model_kwargs: dict = {"torch_dtype": torch.bfloat16},
+        model_kwargs: dict = {
+            "torch_dtype": torch.bfloat16,
+        },
     ):
         super().init_model(model_kwargs=model_kwargs)
 
@@ -105,66 +154,23 @@ class LlamaV38BInstance(BaseLLMInstance):
         return super().read_prompt(source_file_path, template_file_path)
 
     def process_prompt(self, prompt: str, **llm_kwargs):
-        response = self.pipeline(prompt, **llm_kwargs)[0]["generated_text"]
-        return response
-
-    def write_response(self, filepath: str, response: str):
-        super().write_response(filepath, response)
-
-
-class AppleSiliconMixtral8x7BInstance(AppleSiliconBaseLLMInstance):
-    def __init__(self):
-        self.model_info = MODEL_DEF["applesilicon_mixtral-8x7b"]
-        super().__init__(
-            model_name=self.model_info["name"], model_id=self.model_info["hf_id"]
-        )
-
-    def init_model(self):
-        super().init_model()
-
-    def read_prompt(self, source_file_path: str, template_file_path: str):
-        return super().read_prompt(source_file_path, template_file_path)
-
-    def process_prompt(self, prompt: str, **llm_kwargs):
-        # From: https://medium.com/@xuer.chen.human/beginners-guide-to-running-llama-3-8b-on-a-macbook-air-ffb380aeef0c
-        messages = [
-            {"role": "user", "content": prompt},
-        ]
+        messages = [{"role": "user", "content": prompt}]
         input_ids = self.tokenizer.apply_chat_template(messages)
-        prompt_ = self.tokenizer.decode([input_ids[0]])
-        response = mlx_lm.generate(
-            self.model, self.tokenizer, prompt=prompt, **llm_kwargs
-        )
-        return response
 
-    def write_response(self, filepath: str, response: str):
-        super().write_response(filepath, response)
+        if self.device == torch.device("mps"):
+            # Using MLX
+            # From: https://medium.com/@xuer.chen.human/beginners-guide-to-running-llama-3-8b-on-a-macbook-air-ffb380aeef0c
+            prompt = self.tokenizer.decode(input_ids)
+            response = mlx_lm.generate(
+                self.model, self.tokenizer, prompt=prompt, **llm_kwargs
+            )
+        else:
+            # Using transformers
+            # Generate response using the model
+            outputs = self.model.generate(input_ids, **llm_kwargs)
 
-
-class AppleSiliconLlamaV38BInstance(AppleSiliconBaseLLMInstance):
-    def __init__(self):
-        self.model_info = MODEL_DEF["applesilicon_meta-llama-3-8b"]
-        super().__init__(
-            model_name=self.model_info["name"], model_id=self.model_info["hf_id"]
-        )
-
-    def init_model(self):
-        super().init_model()
-
-    def read_prompt(self, source_file_path: str, template_file_path: str):
-        return super().read_prompt(source_file_path, template_file_path)
-
-    def process_prompt(self, prompt: str, **llm_kwargs):
-        # From: https://medium.com/@xuer.chen.human/beginners-guide-to-running-llama-3-8b-on-a-macbook-air-ffb380aeef0c
-
-        messages = [
-            {"role": "user", "content": prompt},
-        ]
-        input_ids = self.tokenizer.apply_chat_template(messages)
-        prompt_ = self.tokenizer.decode([input_ids[0]])
-        response = mlx_lm.generate(
-            self.model, self.tokenizer, prompt=prompt, **llm_kwargs
-        )
+            # Decode and return the generated text
+            response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         return response
 
     def write_response(self, filepath: str, response: str):
@@ -173,11 +179,11 @@ class AppleSiliconLlamaV38BInstance(AppleSiliconBaseLLMInstance):
 
 if __name__ == "__main__":
     # Instantiate the model class with the desired LLM model name
-    model_instance = AppleSiliconMixtral8x7BInstance()
+    model_instance = LlamaV38BInstance(use_mlx_model=True)
 
     # Process prompts given the paths to source and template JSON files
     prompt = "Provide a list of three states of the USA, only the names."
     print(f"PROMPT: \n {prompt}")
     print(
-        f"RESPONSE: \n {model_instance.process_prompt(prompt, temp=0.01, max_tokens=24, verbose=True)}"
+        f"RESPONSE: \n {model_instance.process_prompt(prompt, temp=0.01, max_tokens=100, verbose=True)}"
     )
